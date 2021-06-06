2a2ad1e2a754f5867d8f23d88934c7fa0c61c41e
tush-rastogi <tushar172000@gmail.com>
2021-06-06 12:31:43 +0530

    add search city feature

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ window.addEventListener('load',()=>{
      let sunsettime=document.querySelector(".sunset");
      let hours=document.querySelectorAll("h3");
      let city=document.querySelector(".cityname");
-      
+    // let citysearch=document.querySelector(".citysearch");
+        // let time=0;
 
        const apikey="659662f0657d2bd732d4518c8dfe6944";
        timeupdate();
@@ -31,7 +32,7 @@ window.addEventListener('load',()=>{
         //   console.log(futureapi);
          
               
-       const api=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=${apikey}`;
+       let api=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=${apikey}`;
         console.log(api);
              
         
@@ -83,17 +84,20 @@ window.addEventListener('load',()=>{
                 icons.setAttribute("src",`http://openweathermap.org/img/wn/${icon}@2x.png`);
                  
                  
-                 for(let i=0;i<5;i++)
+                   function fivehours(response)
+                   {
+                for(let i=0;i<5;i++)
                  {
                      
                        let time=response.hourly[i].dt;
                       hours[i].textContent=convertedTime(time);
-                       
-                      let x=`day-${i+1}`;
                     
-
                  }
-                    
+               
+                // console.log(time);
+                }
+
+                    fivehours(response);
                  for(let i=0;i<5;i++)
                  {
                       let a=document.querySelector(`.temp${i+1}`);
@@ -113,8 +117,107 @@ window.addEventListener('load',()=>{
 
                  }
          
-                  
+                 let search=document.querySelector(".button");
+                     search.addEventListener('click',(a)=>{
 
+                    a.preventDefault();
+        // timezone.textContent=cityname.value;
+           let cityname=document.querySelector(".citysearch");
+       
+         const cityapi=`https://api.openweathermap.org/data/2.5/weather?q=${cityname.value}&appid=${apikey}`  
+
+             
+         
+         console.log(cityapi);
+        
+           
+          
+              fetch(cityapi).then(data=>{
+                 
+                 if(data.ok)
+                  return data.json();
+                 
+                else
+                {
+                  alert("City not found");
+                }
+                
+                
+              })
+              .then(response=>{
+      
+            const{sunrise,sunset}=response.sys;
+                  
+            sunrisetime.textContent=sunrise;
+            sunsettime.textContent=convertedTime(sunset);
+            const{lon,lat}=response.coord;
+            console.log(lon, lat);
+
+               api=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${apikey}`;
+                console.log(api);
+               fetch(api).then(data=>data.json()).then(response=>{
+
+                const{timezone}=response;
+                 const{dt}=response.current;
+                 
+                  timezones.textContent=timezone;
+                  
+                    city.textContent=cityname.value; 
+
+                     
+                        const{icon}=response.current.weather[0];
+                        icons.setAttribute("src",`http://openweathermap.org/img/wn/${icon}@2x.png`);
+                 
+
+                        // const{temp}=response.hourly.temp;
+                       let  d = new Date();
+                       let localTime = d.getTime();
+                        let localOffset = d.getTimezoneOffset() * 60000;
+                        let utc = localTime + localOffset;
+                         let y=utc + (1000 * response.timezone_offset);
+                          let citytime=new Date(y);
+                           console.log(citytime);
+                           
+                           usertime.textContent=citytime;
+                            const{temp,humidity,wind_speed}=response.current;
+
+                             temperature.textContent=temp;
+                             const{description}=response.current.weather[0];
+                              status.textContent=description;
+                              humidityy.textContent=humidity;
+                              
+                               speeds.textContent=wind_speed;
+
+                               for(let i=0;i<5;i++)
+                               {
+                                    let a=document.querySelector(`.temp${i+1}`);
+                                    let b=document.querySelector(`.humid${i+1}`);
+                                    let c=document.querySelector(`.speed${i+1}`);
+                                    let d=document.querySelector(`.status${i+1}`);
+                                    let e=document.querySelector(`.img${i+1}`);
+                                 //  console.log(a);
+                                      a.textContent=response.hourly[i].temp;
+              
+                                    b.textContent=response.hourly[i].humidity+"%";
+                                    c.textContent=response.hourly[i].wind_speed;
+                                     d.textContent=response.hourly[i].weather[0].description;
+               
+                                       let ic=response.hourly[i].weather[0].icon;
+                                      e.setAttribute("src",`http://openweathermap.org/img/wn/${ic}@2x.png`)
+              
+                               }
+                       
+
+
+                               
+
+
+               });
+ 
+
+           icons.setAttribute("src",`http://openweathermap.org/img/wn/${icon}@2x.png`);
+   });
+  
 
               });
 
@@ -124,61 +227,25 @@ window.addEventListener('load',()=>{
            fetch(api2).then(data=>data.json()).then(response=>{
 
                  city.textContent=response.name;
-
-           });
+                });
 
 
 
        });
    
           
-    }
+    });
+   }
 
-
-    let cityname=document.querySelector(".city");
-    let search=document.querySelector("button");
      
-          
-       search.addEventListener('click',(a)=>{
-
-             a.preventDefault();
-           timezone.textContent=cityname.value;
-
-            
-            const cityapi=`https://api.openweathermap.org/data/2.5/weather?q=${cityname.value}&appid=${apikey}`  
-
-                   
-             fetch(cityapi).then(data=>data.json()).then(response=>{
-           
-           
-            const {description}=response.weather[0];
-            const{temp,humidity}=response.main;
-            const{speed}=response.wind;
-            const{name}=response;
-            let {icon}=response.weather[0];
-            const{country}=response.sys;
-
-
-             
-
-            status.textContent=description;
-            temperature.textContent=temp;
-            speeds.textContent=speed;
-            humidityy.textContent=humidity+"%";
-            timezone.textContent=name+"/"+country;
-
-            icons.setAttribute("src",`http://openweathermap.org/img/wn/${icon}@2x.png`);
-        });
-
-       });
 
        function timeupdate() {
            let current = new Date();
-           let  time =`${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
+            const time =`${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
           // console.log(time);
             usertime.textContent = time;
           setTimeout(timeupdate, 1000);
-      }     
-   
+        }     
 
-});
+    });
+       
